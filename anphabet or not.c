abdab91c7ef20e96db5d51d5ffbfb36f4820da20
Jeba147 <47944094+Jeba147@users.noreply.github.com>
2019-03-19 19:27:01 +0530

    Create anphabet or not.c

---
+++ anphabet or not.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    char n;
+    scanf("%c",&n);
+    if((n>='a')||(n>='A'))
+    printf("alphabet");
+    else
+    printf("not a alphabet");
+    return 0;
+}
